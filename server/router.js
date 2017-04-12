7d444b7dbe280de4ad26dd277e50e88a736f6c0c
zsnydr <zackwsnyder@gmail.com>
2017-04-12 14:54:46 -0700

    [remove] remove redis dependency for quick heroku test

--- server/router.js
+++ server/router.js
@@ -1,32 +1,32 @@
-const redis = require('redis');
+// const redis = require('redis');
 const genData = require('./genData');
 
-const client = redis.createClient();
+// const client = redis.createClient();
 
-client.on('error', (err) => {
-  console.log(`Error from client: ${err}`);
-});
+// client.on('error', (err) => {
+//   console.log(`Error from client: ${err}`);
+// });
 
-client.on('ready', () => {
-  console.log('Redis connection is ready');
-});
+// client.on('ready', () => {
+//   console.log('Redis connection is ready');
+// });
 
 module.exports = (app) => {
   app.get('/transactions', (req, res) => {
     const { start, end } = req.query;
-    client.get('trans', (err, results) => {
-      if (err) console.log('Error getting data from Redis: ', err);
-      if (results) {
-        let data = JSON.parse(results);
-        data = data.slice(start * 20, end * 20);
-        res.send(data);
-      } else {
+    // client.get('trans', (err, results) => {
+    //   if (err) console.log('Error getting data from Redis: ', err);
+    //   if (results) {
+    //     let data = JSON.parse(results);
+    //     data = data.slice(start * 20, end * 20);
+    //     res.send(data);
+    //   } else {
         const data = genData.slice(start * 20, end * 20);
         res.send(data);
         // now add data to redis
-        const temp = JSON.stringify(genData);
-        client.setex('trans', 600, temp);  // expires in 10 minutes
-      }
-    });
+        // const temp = JSON.stringify(genData);
+        // client.setex('trans', 600, temp);  // expires in 10 minutes
+      // }
+    // });
   });
 };
