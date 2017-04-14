a71b177c445a14b61882ce70ed0cb5bf96986afe
zsnydr <zackwsnyder@gmail.com>
2017-04-13 18:41:35 -0700

    [fix] quick css cleanup in TextInput and TextArea

--- public/app/textArea.jsx
+++ public/app/textArea.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const TextArea = ({ name, label, changeHandler }) => (
+const TextArea = ({ value, name, label, changeHandler }) => (
   <div className="text-input">
     <label htmlFor={name}>
       {label}
       <br />
-      <textarea type="text" id={name} name={name} onChange={changeHandler} />
+      <textarea type="text" id={name} name={name} value={value} onChange={changeHandler} />
     </label>
   </div>
 );
 
 TextArea.propTypes = {
+  value: PropTypes.string,
   name: PropTypes.string,
   label: PropTypes.string,
   changeHandler: PropTypes.func
--- public/app/textInput.jsx
+++ public/app/textInput.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const TextInput = ({ name, label, changeHandler, children }) => (
+const TextInput = ({ value, name, label, changeHandler, children }) => (
   <div className="text-input">
     <label htmlFor={name}>
       {label}
-      <input type="text" id={name} name={name} onChange={changeHandler} />
+      <input type="text" id={name} name={name} value={value} onChange={changeHandler} />
     </label>
     {children}
   </div>
 );
 
 TextInput.propTypes = {
+  value: PropTypes.string,
   name: PropTypes.string,
   label: PropTypes.string,
   changeHandler: PropTypes.func,
